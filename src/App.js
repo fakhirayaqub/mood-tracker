1e9a127b485107c010c196c9f5f2f6cd7d7b3f6e
fakhira <fakhira.yaqub>
2020-12-24 16:25:06 +0100

    added pre next sign

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Collapsible from "./components/Collapsible";
 import Header from "./components/Header"
+import Navigation from "./components/Navigation"
 function App() {
   return (
     <div className="App">
       <Header/>
         <Collapsible />
+        <Navigation/>
       </div>
   
   );
---
+++ src/components/Navigation.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { LeftOutlined , RightOutlined} from '@ant-design/icons';
+
+
+export default function Navigation(){
+
+
+
+
+return(
+  <div>
+    <LeftOutlined />
+    <RightOutlined />
+  </div>
+)
+
+
+
+
+
+}
